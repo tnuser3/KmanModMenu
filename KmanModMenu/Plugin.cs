d3e8303aca0d09f6bc3bd78369c3ba7953a2aca6
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2025-03-20 18:33:06 -0500

    Added Initializer

---
+++ KmanModMenu/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KmanModMenu
+{
+    [BepInPlugin("com.kman.modmenu", "modmenu", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        #region Initializer
+        static bool _initialized = false;
+
+        public void Awake()
+        {
+            if (_initialized) return;
+            var go = new GameObject("KmanModMenu");
+            go.AddComponent<Plugin>();
+            DontDestroyOnLoad(go);
+
+            _initialized= true;
+
+            Destroy(this);
+        }
+        #endregion
+
+        public void LateUpdate()
+        {
+
+        }
+    }
+}

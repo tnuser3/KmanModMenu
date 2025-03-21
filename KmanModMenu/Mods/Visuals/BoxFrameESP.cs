ee5e5223c8cda06858646a30b068d424a9213ef3
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2025-03-20 21:27:49 -0500

    Added visuals

---
+++ KmanModMenu/Mods/Visuals/BoxFrameESP.cs
@@ -0,0 +1,118 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+#endregion
+
+namespace KmanModMenu.Mods.Visual
+{
+    internal class BoxFrameESP
+    {
+        public static Dictionary<VRRig, WFCFing> pairs = new Dictionary<VRRig, WFCFing>();
+
+        public static void Disable()
+        {
+            foreach (var pair in pairs.ToArray())
+                if (pair.Value != null)
+                {
+                    if (pair.Value.GetComponent<MeshFilter>())
+                        pair.Value.GetComponent<MeshFilter>().mesh = null;
+
+                    if (pair.Value.mesh)
+                        Object.Destroy(pair.Value.mesh);
+
+                    if (pair.Value.frameParent)
+                        GameObject.Destroy(pair.Value.frameParent);
+
+                    pairs.Remove(pair.Key);
+                }
+        }
+
+        public static void Execute()
+        {
+            if (Camera.main)
+                Camera.main.cullingMask = -1;
+            foreach (var rig in GorillaParent.instance.vrrigs)
+                if (rig != null && !rig.isOfflineVRRig)
+                {
+                    if (pairs.ContainsKey(rig))
+                    {
+                        if (!pairs[rig].frameParent) continue;
+
+                        if (pairs[rig].frameParent.transform.parent != rig.transform)
+                            pairs[rig].frameParent.transform.SetParent(rig.transform);
+
+                        pairs[rig].wireframeMaterial.color = Color.white;
+                    }
+                    else
+                    {
+                        var ESPGo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        ESPGo.layer = LayerMask.NameToLayer("Temp Rain In City Please Fix Somebody");
+                        var fing = ESPGo.AddComponent<WFCFing>();
+                        pairs.Add(rig, fing);
+                        fing.frameParent = ESPGo;
+                        fing.frameParent.transform.SetParent(rig.transform);
+                        fing.frameParent.transform.localPosition = Vector3.zero;
+                    }
+                }
+                else
+                {
+                    if (pairs.ContainsKey(rig))
+                    {
+                        pairs[rig].GetComponent<MeshFilter>().mesh = null;
+                        Object.Destroy(pairs[rig].mesh);
+                        GameObject.Destroy(pairs[rig].frameParent);
+                        pairs.Remove(rig);
+                    }
+                }
+        }
+
+        [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+        public class WFCFing : MonoBehaviour
+        {
+            public Mesh mesh;
+            public Material wireframeMaterial;
+            public float time;
+            public GameObject frameParent;
+
+            private void Start()
+            {
+                mesh = new Mesh();
+                GetComponent<MeshFilter>().mesh = mesh;
+                cwfb();
+
+                wireframeMaterial = new Material(Shader.Find("GUI/Text Shader"));
+                GetComponent<MeshRenderer>().material = wireframeMaterial;
+                Destroy(GetComponent<BoxCollider>());
+                transform.localScale = new Vector3(0.35f, 0.705f, 0.35f);
+            }
+
+            private void cwfb()
+            {
+                Vector3[] vertices =
+                {
+                    new Vector3(-0.5f, -0.5f, -0.5f),
+                    new Vector3(0.5f, -0.5f, -0.5f),
+                    new Vector3(0.5f, 0.5f, -0.5f),
+                    new Vector3(-0.5f, 0.5f, -0.5f),
+                    new Vector3(-0.5f, -0.5f, 0.5f),
+                    new Vector3(0.5f, -0.5f, 0.5f),
+                    new Vector3(0.5f, 0.5f, 0.5f),
+                    new Vector3(-0.5f, 0.5f, 0.5f)
+                };
+
+                int[] lines =
+                {
+                    0, 1, 1, 2, 2, 3, 3, 0,
+                    4, 5, 5, 6, 6, 7, 7, 4,
+                    0, 4, 1, 5, 2, 6, 3, 7
+                };
+
+                mesh.vertices = vertices;
+                mesh.SetIndices(lines, MeshTopology.Lines, 0);
+            }
+        }
+    }
+}
---
+++ KmanModMenu/Mods/Visuals/Chams.cs
@@ -0,0 +1,70 @@
+﻿#region
+
+using System.Linq;
+using UnityEngine;
+
+#endregion
+
+namespace KmanModMenu.Mods.Visual
+{
+    internal class Chams
+    {
+        public static void Disable()
+        {
+            foreach (var rig in GorillaParent.instance.vrrigs)
+            {
+                if (rig == null || rig.isOfflineVRRig) continue;
+
+                rig.mainSkin.material.color = rig.playerColor;
+                rig.mainSkin.material.shader = Shader.Find("GorillaTag/UberShader");
+                rig.ChangeMaterialLocal(rig.currentMatIndex);
+            }
+        }
+
+        public static void Execute()
+        {
+            if (Camera.main)
+                Camera.main.cullingMask = -1;
+            foreach (var rig in GorillaParent.instance.vrrigs)
+                if (!rig.isOfflineVRRig)
+                {
+                    var fC = new Color(rig.mainSkin.material.color.r, rig.mainSkin.material.color.g,
+                        rig.mainSkin.material.color.b, 0.74f);
+                    rig.mainSkin.material.shader = Shader.Find("GUI/Text Shader");
+                    if (GorillaGameManager.instance is GorillaTagManager)
+                    {
+                        if (rig.mainSkin.material.name.Contains("fected"))
+                            fC = new Color32(255, 111, 0, 190);
+                        else
+                            fC = new Color32(0, 255, 68, 190);
+                    }
+                    else if (GorillaGameManager.instance is GorillaHuntManager)
+                    {
+                        if (rig.setMatIndex == 3)
+                            fC = new Color32(0, 145, 255, 190);
+                        else
+                            fC = new Color32(0, 255, 68, 190);
+                    }
+                    else if (GorillaGameManager.instance is GorillaPaintbrawlManager manager)
+                    {
+                        var status = manager.GetPlayerStatus(rig.Creator);
+                        if ((status & GorillaPaintbrawlManager.PaintbrawlStatus.RedTeam) >
+                            GorillaPaintbrawlManager.PaintbrawlStatus.None)
+                            fC = new Color32(255, 111, 0, 190);
+                        else
+                            fC = new Color32(0, 145, 255, 190);
+                    }
+
+                    if (fC != rig.mainSkin.material.color)
+                    {
+                        var c32array = Enumerable.Repeat((Color32)fC, rig.mainSkin.sharedMesh.colors32.Length)
+                            .ToArray();
+                        var carray = Enumerable.Repeat(fC, rig.mainSkin.sharedMesh.colors32.Length).ToArray();
+                        rig.mainSkin.sharedMesh.colors32 = c32array;
+                        rig.mainSkin.sharedMesh.colors = carray;
+                        rig.mainSkin.material.color = fC;
+                    }
+                }
+        }
+    }
+}

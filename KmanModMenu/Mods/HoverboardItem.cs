f260d9422ad9e5c73b7105b7b7c86ac3b962e2c4
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2025-03-22 18:07:21 -0500

    Update HoverboardItem.cs

--- KmanModMenu/Mods/HoverboardItem.cs
+++ KmanModMenu/Mods/HoverboardItem.cs
@@ -64,40 +64,5 @@ namespace KmanModMenu.Mods
                 }
             }
         }
-
-        public static void GrabBoard()
-        {
-            var pool = Traverse.Create(FreeHoverboardManager.instance).Field("freeBoardPool").GetValue() as Stack<FreeHoverboardInstance>;
-            if (pool != null && pool.Any())
-            {
-                GorillaTagger.Instance.offlineVRRig.enabled = false;
-                GorillaTagger.Instance.offlineVRRig.transform.position = pool.Peek().transform.position;
-                typeof(PhotonNetwork)?.GetMethod("RunViewUpdate", BindingFlags.Static | BindingFlags.NonPublic)?.Invoke(null, Array.Empty<object>());
-                
-                FreeHoverboardManager.instance.photonView.SendUnlimmitedRPC("GrabBoard_RPC", Photon.Pun.RpcTarget.All, [
-                        PhotonNetwork.LocalPlayer.ActorNumber, true, null
-                    ]);
-            }
-        }
-
-        public static void MoveMe()
-        {
-            GorillaTagger.Instance.offlineVRRig.enabled = true;
-            typeof(PhotonNetwork)?.GetMethod("RunViewUpdate", BindingFlags.Static | BindingFlags.NonPublic)?.Invoke(null, Array.Empty<object>());
-
-        }
-
-        public static void DropBoard()
-        {
-
-            FreeHoverboardManager.instance.photonView.SendUnlimmitedRPC("DropBoard_RPC", Photon.Pun.RpcTarget.All, [
-                    true,
-                        BitPackUtils.PackWorldPosForNetwork(GorillaTagger.Instance.offlineVRRig.transform.position),
-                        BitPackUtils.PackQuaternionForNetwork(Quaternion.identity),
-                        BitPackUtils.PackWorldPosForNetwork(Vector3.up),
-                        BitPackUtils.PackWorldPosForNetwork(Vector3.up),
-                        BitPackUtils.PackColorForNetwork(Color.green),
-                    ]);
-        }
     }
 }

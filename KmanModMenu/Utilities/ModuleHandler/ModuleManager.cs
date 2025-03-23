986534dee963f6ee8221ead7b9fa528beac51875
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2025-03-22 19:10:05 -0500

    Added Module stuff

---
+++ KmanModMenu/Utilities/ModuleHandler/ModuleManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmanModMenu.Utilities.ModuleHandler
+{
+    internal class ModuleManager
+    {
+        public class module
+        {
+            public string guid { get; set; }
+            public string creator { get; set; }
+            public string name { get; set; }
+            public string description { get; set; }
+            public string version { get; set; }
+            public Assembly Module { get; set; }
+        }
+
+        public static List<module> getModuleHandles()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (assembly.DefinedTypes.Any())
+                {
+                    foreach (var moduleEntry in assembly.DefinedTypes)
+                    {
+                        Type[] interfaces = moduleEntry.GetInterfaces();
+                        if (interfaces.Contains(typeof(KmanModule.)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ModuleUtil/Module.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmanModule
+{
+    public interface Module
+    {
+        void Init();
+        void Update();
+        void OnLoad();
+        void OnUnload();
+        void UnLoad();
+    }
+}
